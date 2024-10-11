ed7d738084f7f1f5219881952683069a090d516c
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-10-11 23:28:55 +0100

    Delete TaskLists

--- src/components/board/DraggabbleTaskList.tsx
+++ src/components/board/DraggabbleTaskList.tsx
@@ -1,10 +1,19 @@
+import axios, { AxiosError } from "axios";
 import { useEffect, useRef, useState } from "react";
-import TaskList from "./TaskList";
+import { useOutletContext } from "react-router-dom";
+
 import invariant from "tiny-invariant";
 import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import { ITaskList } from "../../interfaces/tasklist";
+import { IOutletContext } from "../../interfaces/outletContext";
+import { baseUrl } from "../../config";
+
+import TaskList from "./TaskList";
+import { toast } from "bulma-toast";
+import { getAxiosErrorMessage } from "../../utils/utils";
 
 function DragabbleTaskList({ list }: { list: ITaskList }) {
+  const { setIsUserRefresh } = useOutletContext<IOutletContext>();
   const ref = useRef(null);
   const [dragging, setDragging] = useState<boolean>(false);
 
@@ -19,9 +28,48 @@ function DragabbleTaskList({ list }: { list: ITaskList }) {
     });
   }, []);
 
+  async function handleDeleteList() {
+    try {
+      const token = localStorage.getItem("token");
+      const URL = `${baseUrl}/tasklists/${list.id}/`;
+      await axios.delete(URL, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setIsUserRefresh(true);
+      toast({
+        message: `Task List deleted`,
+        type: "is-success",
+        dismissible: true,
+        pauseOnHover: true,
+      });
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        const message = getAxiosErrorMessage(e);
+        toast({
+          message: message,
+          type: "is-danger",
+          dismissible: true,
+          pauseOnHover: true,
+        });
+      }
+    }
+  }
+
   return (
     <div className="box" style={dragging ? { opacity: 0.4 } : {}} ref={ref}>
-      <h6 className="is-size-6 pb-3">{list.name}</h6>
+      <nav className="level">
+        <div className="level-left">
+          <div className="level-item">
+            <h6 className="is-size-6">{list.name}</h6>
+          </div>
+        </div>
+        <div className="level-right">
+          <div className="level-item">
+            <button className="delete" onClick={handleDeleteList}></button>
+          </div>
+        </div>
+      </nav>
+
       <TaskList list_id={list.id} />
     </div>
   );
