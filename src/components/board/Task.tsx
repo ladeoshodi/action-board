893c84acfadb392278e210052db0658fe7bb6cc7
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-10-12 00:06:44 +0100

    Mark tasks as done

--- src/components/board/Task.tsx
+++ src/components/board/Task.tsx
@@ -1,6 +1,47 @@
+import { useOutletContext } from "react-router-dom";
 import { ITask } from "../../interfaces/task";
+import { IOutletContext } from "../../interfaces/outletContext";
+import axios, { AxiosError } from "axios";
+import { baseUrl } from "../../config";
+import { toast } from "bulma-toast";
+import { getAxiosErrorMessage } from "../../utils/utils";
 
 function Task({ task }: { task: ITask }) {
+  const { setIsUserRefresh } = useOutletContext<IOutletContext>();
+
+  async function markTaskAsDone() {
+    try {
+      const token = localStorage.getItem("token");
+      const URL = `${baseUrl}/tasks/${task.id}/`;
+
+      const response = await axios.put<ITask>(
+        URL,
+        { done: true },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      setIsUserRefresh(true);
+      toast({
+        message: `${response.data.name} completed`,
+        type: "is-success",
+        dismissible: true,
+        pauseOnHover: true,
+      });
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        const message = getAxiosErrorMessage(e);
+        toast({
+          message: message,
+          type: "is-danger",
+          dismissible: true,
+          pauseOnHover: true,
+        });
+      }
+    }
+  }
+
   return (
     <div className="card has-background-white-ter task-card">
       <div className="card-content">
@@ -42,7 +83,9 @@ function Task({ task }: { task: ITask }) {
         {!task.done && (
           <>
             <a href="#" className="card-footer-item">
-              <div className="has-text-success">Mark as done</div>
+              <div className="has-text-success" onClick={markTaskAsDone}>
+                Mark as done
+              </div>
             </a>
             <a href="#" className="card-footer-item">
               Edit
